a2a6b77b2d1f779abe21c81285adc38e1a836188
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-06-30 17:21:40 +0200

    qpp

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,19 @@
-import Hero from "./Hero";
+import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
-export {Hero, CustomButton, NavBar , Footer}
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
